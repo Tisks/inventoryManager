6fe0f123220cfd62e2d5315b80c775d24c7f1381
Leiser Mahu <leiser.mahu@gmail.com>
2022-12-27 23:05:30 -0300

    Update README.md

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # inventoryManager
 Keep track of your things with this inventory and always know what do you have without even looking at it!
+
+React Native edition
