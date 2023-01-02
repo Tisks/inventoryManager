c72f9163be23869f509e8cb778b33735aca40c4b
Tisks <leiser.mahu@usach.cl>
2023-01-02 18:10:32 -0300

    chore: adding env variables support

---
+++ inventoryManager/src/config/index.ts
@@ -0,0 +1,32 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getDatabase} from 'firebase/database';
+import {getFirestore} from 'firebase/firestore';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  DATABASE_URL,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from '@env';
+
+const config = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  databaseURL: DATABASE_URL,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+const app = initializeApp(config);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const db_rt = getDatabase(app);
+
+export {app, db, auth, db_rt};
