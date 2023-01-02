0847e8d1a8193dbbcc712bc2459c2992e0e3a806
Tisks <leiser.mahu@usach.cl>
2023-01-02 18:09:38 -0300

    feat: adding layout

---
+++ inventoryManager/src/common/layout/Generic.tsx
@@ -0,0 +1,31 @@
+import React, {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const GenericLayout: React.FC<PropsWithChildren> = ({children}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        {children}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default GenericLayout;
