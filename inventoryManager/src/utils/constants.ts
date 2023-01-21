f5cd179a5ccd2254d7af5758c97bb0cd40d4f282
Tisks <leiser.mahu@usach.cl>
2023-01-21 20:52:26 -0300

    chore: email generic regex

--- inventoryManager/src/utils/constants.ts
+++ inventoryManager/src/utils/constants.ts
@@ -9,6 +9,10 @@ export type tabRoute = {
   tab: string;
 };
 
+export const emailPattern = `/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`;
+
+export const emailRegex = RegExp(emailPattern);
+
 export type tabRoutes = tabRoute[][];
 
 export type TCollection = keyof typeof Collection;
