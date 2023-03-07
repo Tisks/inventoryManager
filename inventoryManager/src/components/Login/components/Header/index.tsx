688b9f6e2b24da75a2f1713d1379b1bd26fe67e0
Tisks <leiser.mahu@usach.cl>
2023-03-06 23:04:26 -0300

    chore: react component instead of function

--- inventoryManager/src/components/Login/components/Header/index.tsx
+++ inventoryManager/src/components/Login/components/Header/index.tsx
@@ -1,30 +1,28 @@
-import {Heading} from 'native-base';
 import React from 'react';
+import {Heading} from 'native-base';
 
-const Header = () => {
-  return (
-    <>
-      <Heading
-        size="lg"
-        fontWeight="600"
-        color="coolGray.800"
-        _dark={{
-          color: 'warmGray.50',
-        }}>
-        Welcome
-      </Heading>
-      <Heading
-        mt="1"
-        _dark={{
-          color: 'warmGray.200',
-        }}
-        color="coolGray.600"
-        fontWeight="medium"
-        size="xs">
-        Sign in to continue!
-      </Heading>
-    </>
-  );
-};
+const Header: React.FC = () => (
+  <>
+    <Heading
+      size="lg"
+      fontWeight="600"
+      color="coolGray.800"
+      _dark={{
+        color: 'warmGray.50',
+      }}>
+      Welcome
+    </Heading>
+    <Heading
+      mt="1"
+      _dark={{
+        color: 'warmGray.200',
+      }}
+      color="coolGray.600"
+      fontWeight="medium"
+      size="xs">
+      Sign in to continue!
+    </Heading>
+  </>
+);
 
 export default Header;
